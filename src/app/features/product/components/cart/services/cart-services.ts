9345f0e67f63a2680de83a75e476c2318a6b89dc
mohamed zakaria <mohamed_zakaria@gmail.com>
2026-03-23 07:22:36 +0200

    add: services card

---
+++ src/app/features/product/components/cart/services/cart-services.ts
@@ -0,0 +1,53 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../../../../environment/environment.prod';
+import { Observable } from 'rxjs';
+import { AuthServices } from '../../../../../core/layouts/auth/services/auth-services';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CartServices {
+  private http = inject(HttpClient);
+  private authServices = inject(AuthServices);
+
+  addCard(productId: string): Observable<any> {
+    const headers = {
+      'Content-Type': 'application/json',
+      Authorization: `${this.authServices.getToken()}`,
+    };
+    return this.http.post(environment.baseUrl + 'cart', productId, { headers });
+  }
+
+  updateCard(count: string, productId: string): Observable<any> {
+    const headers = {
+      'Content-Type': 'application/json',
+      Authorization: `${this.authServices.getToken()}`,
+    };
+    return this.http.put(environment.baseUrl + 'cart' + productId, count, { headers });
+  }
+
+    loggedCard():Observable<any> {
+    const headers = {
+    'Content-Type': 'application/json',
+    'Authorization': `${this.authServices.getToken()}`, 
+  };
+   return this.http.get(environment.baseUrl + 'cart',{ headers });
+  }
+
+  removeItem(productId: string): Observable<any> {
+    const headers = {
+      'Content-Type': 'application/json',
+      Authorization: `${this.authServices.getToken()}`,
+    };
+    return this.http.delete(environment.baseUrl + 'cart' + productId, { headers });
+  }
+
+  clearCard(): Observable<any> {
+    const headers = {
+      'Content-Type': 'application/json',
+      Authorization: `${this.authServices.getToken()}`,
+    };
+    return this.http.delete(environment.baseUrl + 'cart', { headers });
+  }
+}
